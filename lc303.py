a145892c0ef897373a818261cda9ab4f9b238ba6
fundamentalprinciple <princedixit931@gmail.com>
2026-01-07 12:49:56 +0000

    clever memoization to speed up sums of subarrays

--- lc303.py
+++ lc303.py
@@ -1,15 +1,40 @@
-#NAIVE:
+def sumPrefix(list, end):
+        s = 0
+        for i in list[0:end]:
+            s += i
+        return s
+
 class NumArray:
     arr = []
-    def __init__(self, nums: List[int]):
+    sums = []
+    def __init__(self, nums: list[int]):
         self.arr = nums
-        return 
+        self.sums = []
+        #MEMOIZING PREFIX SUBARRAY SUMS
+        for i in range(len(self.arr)):
+            self.sums.append(sumPrefix(self.arr, (i+1)))
+        return
 
     def sumRange(self, left: int, right: int) -> int:
+        '''
+        #NAIVE O(N) TIME, O(1) Memmory
         sum = 0
         for i in self.arr[left:right+1]:
             sum+=i
-        return sum    
+        return sum
+        '''
+        #NEED TO FIX ALL THIS HARDCODING
+        if len(self.arr) == 1:
+            return self.arr[0]
+        if left == right:
+            return self.arr[left]
+        if left == 0:
+            return self.sums[right]
+        else:
+            return self.sums[right] - self.sums[left-1]
+        
 
- #MEMOIZED:
-            #TOMMOROW
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
