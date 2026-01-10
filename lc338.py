a00b2336cf82b540a09da9c55d1393f424962e70
fundamentalprinciple <princedixit931@gmail.com>
2026-01-10 13:03:56 +0000

    linear solution to lc338

--- lc338.py
+++ lc338.py
@@ -1,6 +1,16 @@
 class Solution:
-    mem = {}
+
     def countBits(self, n: int) -> List[int]:        
+        #LINEAR SOLUTION
+        result = [0]*(n+1)
+        offset = 1
+        for i in range(1,n+1):
+            if offset*2 == i:
+                offset = offset*2
+            result[i] = 1 + result[i-offset]    
+        return result    
+        
+        '''
         result = []
         for j in range(n+1):
             if len(Solution().mem.keys()) > j:
@@ -14,4 +24,6 @@ class Solution:
                     i = i//2
                 Solution().mem[j] = count_ones
                 result.append(count_ones)
-        return result        
+        return result
+        '''
+
