2fa9282b6fe2e5b182229613301fb0fbb6e689ce
fundamentalprinciple <princedixit931@gmail.com>
2026-01-12 13:56:23 +0000

    floyd's fast and slow pointers, not mutation whatsoever and O(1) memory

--- lc141.py
+++ lc141.py
@@ -6,6 +6,52 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        #Floyd's fast and slow pointers 
+        
+        slow, fast = head, head
+
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False        
+
+
+        '''
+        MORE EXPLICIT CONDITIONAL EVALUATIONS, BAD FOR READABILITY
+
+        if head == None:
+            return False
+        else:
+            a = head    
+
+        if head.next == None or head.next.next == None:
+            return False
+        else:
+            b = head.next.next  
+
+        while(True):
+            if a == b:
+                return True
+
+            if a == None:
+                return False
+            else:
+                a = a.next    
+
+            if b.next == None or b.next.next == None:
+                return False
+            else:
+                b = b.next.next    
+        '''        
+
+
+
+        '''
+        BY MUTATING
+
         element = head
         while(True):  
             if element == None:
@@ -15,3 +61,4 @@ class Solution:
             else:
                 return True      
             element = element.next
+        '''    
