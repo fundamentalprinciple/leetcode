494af788ebe5112123373266f70126fe909b76e5
fundamentalprinciple <princedixit931@gmail.com>
2026-01-22 13:19:41 +0000

    using recursive linked list reversal on second half after reach middle of the linked list

--- lc234.py
+++ lc234.py
@@ -3,24 +3,58 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+def reverse(head):
+    #recursive
+    a = head
+    if head.next.next == None:
+        t = head.next
+        head.next = None
+        t.next = head
+        return t
+    head = reverse(head.next)    
+    if a.next.next == None:
+        t = a.next
+        a.next = None
+        t.next = a
+    return head
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow,fast = head,head
+        #lil bit hardcoding, not a good practice
+        if head == None or head.next == None:
+            return True
+        if head.next.next == None:
+            if head.val == head.next.val:
+                return True
+            else:
+                return False
+
+        lagger,slow,fast = head,head,head
         count = -1
         while(fast and fast.next):
             fast = fast.next.next
             slow = slow.next
-            count+=1  
-        if fast:
-            slow = slow.next   
-        a = head
-        while(slow):
-            for i in range(count):
-                a = a.next
-                count-=1
-            if a.val == slow.val:
-                slow = slow.next  
-            else:
-                return False      
-            a = head 
-        return True 
+            count+=1
+        for i in range(count):
+            lagger=lagger.next
+        lagger.next = reverse(slow)    
+        
+        a, b = head, lagger.next
+
+        if fast: #odd
+            while(b and b.next):
+                if a.val == b.val:
+                    a = a.next
+                    b = b.next
+                else:
+                    return False
+        else:     #even
+            while(b):
+                if a.val == b.val:
+                    a = a.next
+                    b = b.next
+                else:
+                    return False
+        return True            
+
