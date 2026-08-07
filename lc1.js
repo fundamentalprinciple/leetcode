cad9a97433ecf366e79f62040ba0a008b859b143
fundamentalprinciple <princedixit931@gmail.com>
2026-08-07 18:59:04 +0800

    algorithm limit reached, need to read solutions

--- lc1.js
+++ lc1.js
@@ -4,8 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // faulty algorithm, can't be improved further
+    /*
+    Checks for adjacent elements
     i=0
     j=1
+    //nums.sort((a,b)=>{return a-b}) // array.sort() method works strings
     while (j!=nums.length+1) {
         if (target-nums[i]-nums[j]==0) {
                 return [i,j];
@@ -14,4 +18,20 @@ var twoSum = function(nums, target) {
             j++;
         };
     };
+
+    Checks for same element apperaing twice
+    i=0
+    j=1
+    nums.sort((a,b)=>{return a-b})
+    while (j!=nums.length+1) {
+        if (target-nums[i]-nums[j]==0) {
+                return [i,j];
+        } else {
+            i++;
+            j++;
+        };
+    };
+
+    Misses non-adjacent element solutions
+    */
 };
